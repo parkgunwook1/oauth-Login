ed7ff3e95b13023187cd53a0c78cbffeb2628741
parkgunwook1 <pkw85428@naver.com>
2024-01-28 22:54:17 +0900

    springboot maven 세팅

---
+++ login/src/main/java/oauth/login/config/SpringSecurityConfig.java
@@ -0,0 +1,16 @@
+package oauth.login.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // @Bean을 사용해 수동으로 등록할 수 있다.
+public class SecurityConfigration {
+
+
+}
